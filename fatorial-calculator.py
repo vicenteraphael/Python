bc29af5c5e03023ecb2529bde0d9b2d501f5b464
Raphael Vicente <rafaelx9.oliveira@gmail.com>
2024-05-01 15:48:54 -0300

    final update on fatorial-calculator

--- fatorial-calculator.py
+++ fatorial-calculator.py
@@ -1,17 +1,19 @@
 print ("\n")
-print ("Bem-vindo à Calculadora Fatorial do Raphael Vicente"); print ("\n")
+print ("Bem-vindo à Calculadora de Fatorial do Raphael Vicente"); print ("\n")
 num = ""
 while num != "s":
     print ("*para sair, digite 's'* \nEntre com o número inteiro para fatorial:"); print ("\n")
     num = input ()
+    while num.isnumeric() == False:
+        print ("\n")
+        print ("Entre com um número inteiro! \n*para sair, digite 's'*"); print ("\n")
+        num = input ()
+        if num == "s":
+            break
     if num == "s":
         print ("\n")
         print ("Adeus!")
         break
-    while num.isnumeric() == False:
-        print ("\n")
-        print ("Entre com um número inteiro!")
-        num = input ()
     fat = num
     cont = int(num) - 1
     for cont in range (int(cont), 1, -1):
