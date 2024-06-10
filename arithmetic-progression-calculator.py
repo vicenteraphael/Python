cb5b7096a9053a3ae965446f4f385d3c7b277755
Raphael Vicente <rafaelx9.oliveira@gmail.com>
2024-06-10 15:39:58 -0300

    fixes: arithmetic progression calculator

--- arithmetic-progression-calculator.py
+++ arithmetic-progression-calculator.py
@@ -3,200 +3,193 @@ Variáveis e Funções:
 a = 1º termo (a1)
 b = 2º termo (an); razão (r); último termo (an)
 n = escolha do menu; índice do termo desejado (n de an); índice do último termo (n de an)
-num_invalido()
-msg_adeus()
 """
-
-def num_invalido():
-    print ("\n")
-    print ("*número inválido*")
-def msg_adeus():
-    print ("\n")
-    print ("Adeus!")
+n = ''; a = ''; b = ''
 
 # Boas-Vindas
 
-print ("\n")
-print ("Bem-vindo(a) à Calculadora de Progressão Aritmética do Raphael"); print ("\n")
-print ("Para inciar, pressione 'Enter' \nPara sair, digite 's'"); print ("\n")
-n = input ()
-if n == "s":
-    msg_adeus()
+print ("\nBem-vindo(a) à Calculadora de Progressão Aritmética do Raphael\n")
 
     # Menu de opções:
 
-while n != "s":
-    print ("Você deseja encontrar:"); print ("\n")
-    print ("1) Um termo n da progressão (an) \n2) A razão da progressão (r) \n3) A soma geral dos termos da progressão (Sn) \n4) Sair"); print ("\n")
+while n != "s" and a != "s" and b != "s":
+    print ("\nVocê deseja calcular: \n\n1) Todos os termos da progressão \n2) Um termo n da progressão (an) \n3) A razão da progressão (r) \n4) A soma geral dos termos da progressão (Sn) \n5) Sair\n")
     n = input ()
-    while n != "1" and n != "2" and n != "3" and n != "4":
-        print ("\n")
-        print ("*alternativa inválida*")
-        print ("Você deseja encontrar:"); print ("\n")
-        print ("1) Um termo n da progressão (an) \n2) A razão da progressão (r) \n3) A soma geral dos termos da progressão (sn) \n4) Sair"); print ("\n")
+    while n != "1" and n != "2" and n != "3" and n != "4" and n != "5" and n != "s":
+        print ("\n*alternativa inválida*")
+        print ("Você deseja calcular: \n\n1) Todos os termos da progressão \n2) Um termo n da progressão (an) \n3) A razão da progressão (r) \n4) A soma geral dos termos da progressão (Sn) \n5) Sair\n")
         n = input ()
-    if n == "4":
-        msg_adeus()
+    if n == "5" or n == "s":
         break
 
-    # Razão da progressão:
+    # Todos os termos da progressão:
 
-    elif n == "2":
-        print ("\n")
-        print ("Informe-me dois termos consecutivos da progressão: \n*para sair, digite 's'*"); print ("\n")
-        while True:
-            try:
-                a = input ("1º termo: ")
-                if a == "s":
-                    break
-                else:
+    if n == "1":
+        print ("\n*Para sair, entre com 's'. Para voltar ao menu de opções, entre com 'm'* \nInforme-me:\n")
+        while n == "1":
+            while True:
+                try:
+                    a = input ("1º termo da progressão (a1): ")
+                    if a == "s" or a == "m":
+                        break
                     a = float(a)
                     break
-            except ValueError or TypeError:
-                print ("*entre com números!* \n*para sair, digite 's'*")
-                if a == "s":
-                    break
-        if a == "s":
-            msg_adeus()
-            break
-        while True:
-            try:
-                b = input ("2º termo: ")
-                if b == "s":
-                    break
-                else:
+                except ValueError or TypeError:
+                    print ("Entre com um número!")                
+            if a == "s" or a == "m":
+                break
+            while True:
+                try:
+                    b = input ("Último termo da progressão (an): ")
+                    if b == "s" or b == "m":
+                        break
                     b = float(b)
                     break
-            except ValueError or TypeError:
-                print ("*entre com números!* \n*para sair, digite 's'*")
-                if b == "s":
+                except ValueError or TypeError:
+                    print ("Entre com um número!")                
+            if b == "s" or b == "m":
+                break
+            while True:
+                try:
+                    n = input ("Razão da progressão (r): ")
+                    if n == "s" or n == "m":
+                        break
+                    n = float(n)
                     break
-        if b == "s":
-            msg_adeus()
-            break
+                except ValueError or TypeError:
+                    print ("Entre com um número!")                
+            if n == "s" or n == "m":
+                break
 
-        # Saída:
+            # Saída:
 
-        print ("\n")
-        print ("r =", b - a)
+            print ("\nTodos os termos: ")
+            while a <= b:
+                if a == b:
+                    print ("{}".format(a))
+                elif a == b - n:
+                    print ("{} e ".format(a), end='')
+                else:
+                    print ("{}, ".format(a), end='')
+                a += n
+            n = "1"; print ("")
 
     # Termo n da progressão (an):
 
-    elif n == "1":
-        print ("\n")
-        print ("*para sair, digite 's'* \nInforme-me:"); print ("\n")
-        while True:
-            try:
-                a = input ("1º termo da progressão (a1): ")
-                if a == "s":
-                    break
-                else:
+    elif n == "2":
+        print ("\n*Para sair, entre com 's'. Para voltar ao menu de opções, entre com 'm'* \nInforme-me:\n")
+        while n == "2":
+            while True:
+                try:
+                    a = input ("1º termo da progressão (a1): ")
+                    if a == "s" or a == "m":
+                        break
                     a = float(a)
                     break
-            except ValueError or TypeError:
-                print ("*entre com números!* \n*para sair, digite 's'*")
-                if a == "s":
-                    break
-        if a == "s":
-            msg_adeus()
-            break
-        while True:
-            try:
-                b = input ("razão da progressão (r): ")
-                if b == "s":
-                    break
-                else:
+                except ValueError or TypeError:
+                    print ("Entre com um número!")                
+            if a == "s" or a == "m":
+                break
+            while True:
+                try:
+                    b = input ("Razão da progressão (r): ")
+                    if b == "s" or b == "m":
+                        break
                     b = float(b)
                     break
-            except ValueError or TypeError:
-                print ("*entre com números!* \n*para sair, digite 's'*")
-                if b == "s":
-                    break
-        if b == "s":
-            msg_adeus()
-            break
-        while True:
-            try:
-                n = input ("índice do termo desejado (n de an): ")
-                if n == "s":
-                    break
-                else:
+                except ValueError or TypeError:
+                    print ("Entre com um número!")                
+            if b == "s" or b == "m":
+                break
+            while True:
+                try:
+                    n = input ("Índice do termo desejado (n de an): ")
+                    if n == "s" or n == "m":
+                        break
                     n = float(n)
-            except ValueError or TypeError:
-                print ("*entre com números!* \n*para sair, digite 's'*")
-                if n == "s":
                     break
-        if n == "s":
-            msg_adeus()
-            break
+                except ValueError or TypeError:
+                    print ("Entre com um número!")                
+            if n == "s" or n == "m":
+                break
 
-        # Saída:
+            # Saída:
 
-        print ("\n")
-        print ("an =", a + (n - 1) * b)
+            print ("\nan =", a + (n - 1) * b); n = "2"; print ("")
+
+    # Razão da progressão:
+
+    if n == "3":
+        print ("\n*Para sair, entre com 's'. Para voltar ao menu de opções, entre com 'm'*\nInforme-me dois termos consecutivos da progressão:\n")
+        while n == "3":
+            while True:
+                try:
+                    a = input ("1º termo: ")
+                    if a == "s" or a == "m":
+                        break
+                    a = float(a)
+                    break
+                except ValueError or TypeError:
+                    print ("Entre com um número!")                
+            if a == "s" or a == "m":
+                break
+            while True:
+                try:
+                    b = input ("2º termo: ")
+                    if b == "s" or b == "m":
+                        break
+                    b = float(b)
+                    break
+                except ValueError or TypeError:
+                    print ("Entre com um número!")                
+            if b == "s" or b == "m":
+                break
+
+            # Saída:
+
+            print ("\nr =", b - a); n = "3"; print ("")
     
     # Soma geral dos termos (Sn):
 
-    elif n == "3":
-
-        print ("\n")
-        print ("*para sair, digite 's'* \nInforme-me:"); print ("\n")
-        while True:
-            try:
-                a = input ("1º termo da progressão (a1): ")
-                if a == "s":
-                    break
-                else:
+    if n == "4":
+        print ("\n*Para sair, entre com 's'. Para voltar ao menu de opções, entre com 'm'* \nInforme-me:\n")
+        while n == "4":
+            while True:
+                try:
+                    a = input ("1º termo da progressão (a1): ")
+                    if a == "s" or a == "m":
+                        break
                     a = float(a)
                     break
-            except ValueError or TypeError:
-                print ("*entre com números!* \n*para sair, digite 's'*")
-                if a == "s":
+                except ValueError or TypeError:
+                    print ("Entre com um número!")                
+            if a == "s" or a == "m":
+                break
+            while True:
+                try:
+                    b = input ("Último termo da progressão (an): ")
+                    if b == "s" or b == "m":
+                        break
+                    b = float(b)
                     break
-        if a == "s":
-            msg_adeus()
-            break
-        while True:
-            try:
-                b = input ("Último termo da progressão (an): ")
-                if b == "s":
-                    break
-                else:
-                    b = float(n)
-                    break
-            except ValueError or TypeError:
-                print ("*entre com números!* \n*para sair, digite 's'*")
-                if b == "s":
-                    break
-        if b == "s":
-            msg_adeus()
-            break
-        while True:
-            try:
-                n = input ("Índice do último termo da progressão (n de an): ")
-                if n == "s":
-                    break
-                else:
+                except ValueError or TypeError:
+                    print ("Entre com um número!")                
+            if b == "s" or b == "m":
+                break
+            while True:
+                try:
+                    n = input ("Índice do último termo da progressão (n de an): ")
+                    if n == "s" or n == "m":
+                        break
                     n = float(n)
                     break
-            except ValueError or TypeError:
-                print ("*entre com números!* \n*para sair, digite 's'*")
-                if n == "s":
-                    break
-        if n == "s":
-            msg_adeus()
-            break
+                except ValueError or TypeError:
+                    print ("Entre com um número!")                
+            if n == "s" or n == "m":
+                break
 
-        # Saída:
+            # Saída:
 
-        print ("\n")
-        print ("Sn =", ((a + b) * n ) / 2)
+            print ("\nSn =", ((a + b) * n ) / 2); n = "4"; print ("")
 
-    # Sair ou voltar ao Menu de opções:
-
-    print ("\n")
-    print ("Para voltar, pressione 'Enter' \nPara sair, entre com o número 4 ou com a letra 's'"); print ("\n")
-    n = input ()
-    if n == "4":
-        msg_adeus()
-        break
+print ("\nAdeus!")
